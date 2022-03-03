d29940293471d1a7c119d78c695c4df0a225fe89
honghao lyu <honghaol@kth.se>
2022-03-03 01:25:20 +0100

    modified the agent to two independent threads and fixed the first message  bug

--- QuoteForwardAgentCoorThread.java
+++ QuoteForwardAgentCoorThread.java
@@ -10,6 +10,37 @@ import java.net.*;
 public class QuoteForwardAgentCoorThread {
 
     public static void main(String[] args) throws InterruptedException {
+
+        DatagramSocket socket_in;
+        DatagramSocket socket_out;
+        try {
+            socket_in = new DatagramSocket(18);
+            byte[] buffer_recv = new byte[512];
+            DatagramPacket request = new DatagramPacket(buffer_recv, buffer_recv.length);
+            socket_in.receive(request);
+
+            String mag_recv = new String(buffer_recv, 0, request.getLength());
+            System.out.println("client request msg recived: " + mag_recv);
+            socket_in.close();
+
+            // Thread.sleep(10);
+
+            socket_out = new DatagramSocket(17);
+            byte[] buffer = mag_recv.getBytes();
+            InetAddress clientAddress = InetAddress.getByName("192.168.30.55");
+            int clientPort = 17;
+            DatagramPacket response = new DatagramPacket(buffer, buffer.length, clientAddress, clientPort);
+            socket_out.send(response);
+
+            String quote_msg_res = new String(buffer, 0, response.getLength());
+            System.out.println(quote_msg_res);
+            socket_out.close();
+        } catch (SocketException ex) {
+            System.out.println("Socket error: " + ex.getMessage());
+        } catch (IOException ex) {
+            System.out.println("I/O error: " + ex.getMessage());
+        }
+
         final MyData data = new MyData();  
         for(int i=0;i<1;i++){  
             new Thread(new Runnable(){
