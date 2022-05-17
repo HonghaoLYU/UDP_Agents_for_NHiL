63b4762e5f545ddf6603a1416959f3d55916610e
honghao lyu <honghaol@kth.se>
2022-05-17 19:14:04 +0200

    add time sleep to 5G thread

--- QuoteForwardAgentTracThread5G.java
+++ QuoteForwardAgentTracThread5G.java
@@ -37,7 +37,7 @@ public class QuoteForwardAgentTracThread5G {
             String quote_msg_res = new String(buffer, 0, response.getLength());
             System.out.println(quote_msg_res);
             socket_out.close();
-            Thread.sleep(80);
+            Thread.sleep(200);
         } catch (SocketException ex) {
             System.out.println("Socket error: " + ex.getMessage());
         } catch (IOException ex) {
